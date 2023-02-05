5273f8ebfdefa4f6c8f8cab270c75f27267554d5
Bella3009 <120841781+Bella3009@users.noreply.github.com>
2023-02-05 16:24:34 +0100

    Update DocGenerator.py
    Adding footer in every page

--- DocGenerator.py
+++ DocGenerator.py
@@ -3,6 +3,8 @@ import pandas as pd
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
+pdf.set_auto_page_break(auto=False, margin=0)
+
 df = pd.read_csv("Titles.csv")
 
 for index, row in df.iterrows():
@@ -14,6 +16,20 @@ for index, row in df.iterrows():
     # pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=1)
     
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
+    # .line(x1, y1, x2, y2) x rapresent the horizontal line while y is vertical. 1 is from the left and 2 from the right
+    
     pdf.line(10, 21, 200, 21) # To add a simple line
+    pdf.ln(265)
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(180,180,180)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+    
+    for i in range(row["Pages"]-1):
+        pdf.add_page()
+        
+        pdf.ln(275)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180,180,180)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
 pdf.output("FirstPDF.pdf")
